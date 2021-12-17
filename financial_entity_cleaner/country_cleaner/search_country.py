1b7196889103af3556acf774d6e4f97db67df3c1
Patrycia Barros Pereira <patryciabp@gmail.com>
2021-12-17 16:38:48 +0000

    [FIX] Fix the search in the country's dictionary, as to look the internal one, not the internet

--- financial_entity_cleaner/country_cleaner/search_country.py
+++ financial_entity_cleaner/country_cleaner/search_country.py
@@ -1,48 +1,54 @@
 from hdx.location.country import Country
 
 
-def get_name_from_alpha2(country):
+def get_name_from_alpha2(country, lookup_dict_from_web=False):
     """
     Gets the name of a country given its alpha2 code.
 
     Parameters:
         country (str): the alpha2 code
+        lookup_dict_from_web (bool): indicates if the country lib looks up for the updated dictionary
+            of countries available in the web
     Returns:
         country_name (str): the name of the country or None
     Raises:
         No exception is raised.
     """
 
-    country_name = Country.get_country_name_from_iso2(country, use_live=False)
+    country_name = Country.get_country_name_from_iso2(country, use_live=lookup_dict_from_web)
     if not country_name:
         return None
     return country_name
 
 
-def get_name_from_alpha3(country):
+def get_name_from_alpha3(country, lookup_dict_from_web=False):
     """
     Gets the name of a country given its alpha3 code.
 
     Parameters:
         country (str): the alpha3 code
+        lookup_dict_from_web (bool): indicates if the country lib looks up for the updated dictionary
+            of countries available in the web
     Returns:
         country_name (str): the name of the country or None
     Raises:
         No exception is raised.
     """
 
-    country_name = Country.get_country_name_from_iso3(country, use_live=False)
+    country_name = Country.get_country_name_from_iso3(country, use_live=lookup_dict_from_web)
     if not country_name:
         return None
     return country_name
 
 
-def get_alpha3_from_name(country):
+def get_alpha3_from_name(country, lookup_dict_from_web=False):
     """
     Gets the alpha3 code of a country given its name.
 
     Parameters:
         country (str): the name of a country
+        lookup_dict_from_web (bool): indicates if the country lib looks up for the updated dictionary
+            of countries available in the web
     Returns:
         alpha_code (str): the alpha3 code of the country or None
     Raises:
@@ -50,14 +56,14 @@ def get_alpha3_from_name(country):
     """
 
     # First, the function looks for an exact match
-    alpha_code = Country.get_iso3_country_code(country)
+    alpha_code = Country.get_iso3_country_code(country, use_live=lookup_dict_from_web)
 
     # Returns the alpha3 code if the country was found
     if alpha_code:
         return alpha_code
 
     # The exact search didn't work, execute the fuzzy search available in the api
-    alpha_code = Country.get_iso3_country_code_fuzzy(country)
+    alpha_code = Country.get_iso3_country_code_fuzzy(country, use_live=lookup_dict_from_web)
 
     # The country api returns a tuple in which the element [0] is the country code and
     # the element [1] is a True or False result for the search
@@ -67,12 +73,14 @@ def get_alpha3_from_name(country):
     return alpha_code[0]
 
 
-def get_alpha2_from_name(country):
+def get_alpha2_from_name(country, lookup_dict_from_web=False):
     """
     Gets the alpha2 code of a country given its name.
 
     Parameters:
         country (str): the name of a country
+        lookup_dict_from_web (bool): indicates if the country lib looks up for the updated dictionary
+            of countries available in the web
     Returns:
         alpha_code (str): the alpha2 code of the country or None
     Raises:
@@ -81,33 +89,35 @@ def get_alpha2_from_name(country):
 
     # Because there isn't a direct way to get the alpha2 code from the country's name,
     # the function looks for the alpha3 code first and then for the alpha2 code.
-    alpha_code = get_alpha3_from_name(country)
+    alpha_code = get_alpha3_from_name(country, lookup_dict_from_web)
 
     # If the alpha3 code was not found, return nan
     if not alpha_code:
         return None
 
     # Alpha3 was found, therefore get the alpha2 code
-    alpha_code = get_alpha2_from_alpha3(alpha_code)
+    alpha_code = get_alpha2_from_alpha3(alpha_code, lookup_dict_from_web)
     if not alpha_code:
         return None
 
     return alpha_code
 
 
-def get_alpha2_from_alpha3(country):
+def get_alpha2_from_alpha3(country, lookup_dict_from_web=False):
     """
     Gets the alpha2 code of a country given its alpha3 code.
 
     Parameters:
         country (str): the alpha3 code of a country
+        lookup_dict_from_web (bool): indicates if the country lib looks up for the updated dictionary
+            of countries available in the web
     Returns:
         alpha_code (str): the alpha2 code of the country or None
     Raises:
         No exception is raised.
     """
 
-    alpha_code = Country.get_country_info_from_iso3(country)
+    alpha_code = Country.get_country_info_from_iso3(country, use_live=lookup_dict_from_web)
 
     # The api returns a dictionary if the country is found
     if not alpha_code:
